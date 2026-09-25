fdfd2edd138b53c6443fcfb3ddaf88aa6436417e
kokerax <kokerax@users.noreply.github.com>
2026-09-25 19:17:31 +0300

    Subscribe Instagram accounts to ad referral webhooks

--- lib/meta/client.ts
+++ lib/meta/client.ts
@@ -837,7 +837,7 @@ export async function subscribeInstagramAccountToWebhooks(
         Authorization: `Bearer ${accessToken}`,
       },
       body: JSON.stringify({
-        subscribed_fields: ["comments", "messages"],
+        subscribed_fields: ["comments", "messages", "messaging_referral"],
       }),
     }
   );
